da7544cc971ae25b2566c626a0ad126852a8b34a
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2026-07-28 13:47:14 +0300

    feat: add cord nvim to share activity on discord

---
+++ nvim/lua/plugins/cord.lua
@@ -0,0 +1,9 @@
+return {
+  "vyfor/cord.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- Add your custom cord configuration options here, e.g.:
+    -- timer = { interval = 1500 },
+    -- text = { workspace = function(opts) return "In " .. opts.workspace end },
+  },
+}
